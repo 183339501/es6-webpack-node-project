d0e6ce4e3b7daf09b711927a9cf3f6353c4ebb63
py <pengyao_2@163.com>
2016-05-28 18:47:00 +0800

    增加防xss攻击

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -3,12 +3,12 @@
  */
 import marked from 'marked';
 import HighLight from "highlight.js";
-
+import xss from "xss";
 marked.setOptions({
     highlight: function (code) {
         return HighLight.highlightAuto(code).value;
     }
 });
 export function markdownParse(text) {
-    return marked(text);
+    return xss(marked(text));
 }
