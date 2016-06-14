59cccd8ce9e2f7a21fbc7d7e96e2c5982c780a69
py <pengyao_2@163.com>
2016-06-15 00:08:12 +0800

    单元测试案例

--- src/test/test_use.js
+++ src/test/test_use.js
@@ -10,22 +10,23 @@ describe("user",function (){
         try {
             const ret = await request.post('/api/signup', {
                 name: 'test1',
-                password: '123456789',
+                password: '123456789'
             });
             throw new Error('should throws missing parameter "email" error');
         } catch (err) {
-            expect(err.message).to.equal('email: missing parameter "email"');
+            console.log(err,"***********");
+            expect(err).to.equal('email: missing parameter "email"');
         }
 
-        {
-            const ret = await request.post('/api/signup', {
-                name: 'test1',
-                password: '123456789',
-                email: 'test1@example.com'
-            });
-            console.log(ret);
-            //expect(ret.user.name).to.equal('test1');
-            //expect(ret.user.email).to.equal('test1@example.com');
-        }
+        //{
+        //    const ret = await request.post('/api/signup', {
+        //        name: 'test1',
+        //        password: '123456789',
+        //        email: 'test1@example.com'
+        //    });
+        //    console.log(ret);
+        //    //expect(ret.user.name).to.equal('test1');
+        //    //expect(ret.user.email).to.equal('test1@example.com');
+        //}
     });
 })
